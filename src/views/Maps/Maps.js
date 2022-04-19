52160a800eca5e327e96b5393c41e49c0257790f
Nathan Jesus <nathan.jesus@conductor.com.br>
2022-04-19 19:45:02 -0300

    add mapa modelo e desfixa plugin

--- src/views/Maps/Maps.js
+++ src/views/Maps/Maps.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "../../assets/css/maps.css";
+import rotas from "assets/img/rotas-acessiveis/todas-as-rotas.jpg";
 
 function Maps() {
   return(
     <div className="divMapsPage">
-      <h3>Bom dia</h3>
+      <div className="divMapsTitle">
+        <h3>Todas as Rotas</h3>
+      </div>
+      <div className="divMapsTitle">
+        <img src={rotas} alt="Imagem com todas as rotas" />
+      </div>
     </div>
   )
 }
